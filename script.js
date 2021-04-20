d08eb08b2d54a81ad2914099c979ff1f4267040a
GalinaSherbak84 <galinasherbak@gmail.com>
2021-04-20 17:05:56 +0200

    Aggiungere alla pagina un’immagine

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('vue', Vue);
+ const app = new Vue (
+    {
+        el: '#root',
+        data: {
+            title: 'London',
+            saluto: 'Ciao!!!!!',
+            myClass: 'blue',
+            myImg: 'https://www.nyhabitat.com/it/blog/wp-content/uploads/2015/05/Top-10-londra-estate-famiglie-londra-orizzonte.jpg'
+        }
+    }
+ ); 
